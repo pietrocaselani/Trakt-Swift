cfa8dc14db74f47666336a8ca927034e0970d1f5
Pietro Caselani <pc1992@gmail.com>
2018-07-27 06:19:44 -0300

    Add constructor to Genre model

--- Trakt/Models/Genre.swift
+++ Trakt/Models/Genre.swift
@@ -2,6 +2,11 @@ public final class Genre: Codable, Hashable {
   public let name: String
   public let slug: String
 
+  public init(name: String, slug: String) {
+    self.name = name
+    self.slug = slug
+  }
+
   public var hashValue: Int {
     return name.hashValue ^ slug.hashValue
   }
