1b055b6bab843f07cb96ee6378ad040a9751daa6
Pietro Caselani <pc1992@gmail.com>
2018-07-27 05:18:55 -0300

    Fix test bundle locator

--- Trakt/BundleExtensions.swift
+++ Trakt/BundleExtensions.swift
@@ -1,8 +1,8 @@
 extension Bundle {
   static var testing: Bundle {
-    let bundle = Bundle(identifier: "io.github.pietrocaselani.TraktTests")!
+    let bundle = Bundle(for: Trakt.self)
 
-    let path = bundle.bundlePath.appending("/../TraktTestsResources.bundle")
+    let path = bundle.bundlePath.appending("/../../../../TraktTestsResources.bundle")
     let testingBundle = Bundle(path: path)
 
     return testingBundle!
