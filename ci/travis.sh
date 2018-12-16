ba57c2097c0c1f304d44c381bf002369e706fbef
Pietro Caselani <pc1992@gmail.com>
2018-12-16 20:02:42 -0200

    Remove sonar

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Running master.sh"
-    ./ci/master.sh
-else
     echo "Running pull_request.sh"
     ./ci/pull_request.sh
+else
+    echo "Running master.sh"
+    ./ci/master.sh
 fi
